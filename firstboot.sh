ef3da416f7885b89fc49263b3edf6d047c1f7e7a
Prashant Solanki <solankisrp2@gmail.com>
2022-08-28 15:14:06 +0000

    CIFS share

--- firstboot.sh
+++ firstboot.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 
+# Variables
+share_name="{Replace:Name of the CIFS share}"
+share_ip="{Replace:IP of the CIFS server}"
+shared_dir="{Replace:Path to mount the share}"
+shared_dir_cifs_username="{Replace: CIFS username}"
+shared_dir_cifs_password="{Replace:CIFS password}"
+shared_dir_credentials_path="{Replace:Path to store the credentials}"
+ubuntu_password="{Replace:ubuntu user password}"
+
+
 apt-get update -y
-apt-get install -y ca-certificates curl gnupg lsb-release
+apt-get install -y ca-certificates curl gnupg lsb-release cifs-utils
+
+mkdir -p $shared_dir
+
+echo "username=$shared_dir_cifs_username \
+password=$shared_dir_cifs_password" > $shared_dir_credentials
+chmod 400 $shared_dir_credentials
+
+
+echo "//$share_ip/$share_name $shared_dir cifs vers=3.0,credentials=$shared_dir_credentials" >> /etc/fstab
+
+
 
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -17,5 +38,6 @@ apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 usermod -aG docker ubuntu # 2>&1 | tee usermod-docker.output
 # echo "sudo usermod -aG docker $USER" 2>&1 | tee sudo-usermod-docker.output
 
-echo "ubuntu:{Replace:password}" | sudo chpasswd # 2>&1 | tee chpasswd.output
+echo "ubuntu:$ubuntu_password" | sudo chpasswd # 2>&1 | tee chpasswd.output
 docker run hello-world 2>&1 | tee  hello-world.output
+
