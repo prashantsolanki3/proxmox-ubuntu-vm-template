fce08e682d1dcd85711201eca30109fab4b3cea5
Prashant Solanki <solankisrp2@gmail.com>
2023-05-26 15:39:53 +0000

    Remove unnecessary

--- create-template.sh
+++ create-template.sh
@@ -9,7 +9,7 @@ cd $work_dir
 
 wget -nc -O $image_name.original $image_download_url
 cp $image_name.original $image_name
-virt-customize -a $image_name --install qemu-guest-agent,ca-certificates,curl,gnupg,lsb-release
+virt-customize -a $image_name --install qemu-guest-agent
 #virt-customize -a $image_name --firstboot $curr_dir/firstboot.sh
 qm create $vm_id --name $vm_name --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
 qm importdisk $vm_id $image_name $vm_storage 
