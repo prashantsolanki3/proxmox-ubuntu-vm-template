b6e237b5fc9c6a554eb4a2116e4cb52a85700529
Prashant Solanki <solankisrp2@gmail.com>
2023-05-26 16:07:33 +0000

    Remove original file and remove virt-cutomize

--- create-template.sh
+++ create-template.sh
@@ -11,6 +11,7 @@ wget -nc -O $image_name.original $image_download_url
 cp $image_name.original $image_name
 virt-customize -a $image_name --install qemu-guest-agent
 #virt-customize -a $image_name --firstboot $curr_dir/firstboot.sh
+
 qm create $vm_id --name $vm_name --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
 qm importdisk $vm_id $image_name $vm_storage 
 qm set $vm_id --scsihw virtio-scsi-pci --scsi0 $vm_storage:vm-$vm_id-disk-0
@@ -19,4 +20,4 @@ qm set $vm_id --ide2 $vm_storage:cloudinit
 qm set $vm_id --serial0 socket --vga serial0
 qm set $vm_id --agent enabled=1
 qm template $vm_id
-rm $image_name
+rm -f $image_name.original
